c7e289bbc3de536fabf27e4c82170cdf1cb925f9
Bruno Reviglio <brunoareviglio@csus.edu>
2022-11-08 17:01:59 -0800

    [brunoareviglio] build tree

--- src/main/java/edu/csus/csc131/typeahead/data/TrieImpl.java
+++ src/main/java/edu/csus/csc131/typeahead/data/TrieImpl.java
@@ -3,6 +3,7 @@ package edu.csus.csc131.typeahead.data;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,9 +22,49 @@ public class TrieImpl extends Trie {
 	@Override
 	Node buildTree(String str) {
 		logger.trace("buildTree started");
+		StringTokenizer st = new StringTokenizer(str," ");
+		int wordCount = st.countTokens();
+		String arr[];
+		arr = new String[wordCount];
+		String words = String.valueOf(wordCount);
+		logger.trace("Number of words in the document: " + words);
+		//Loads the Tokens into an array
+		for(int i =0; i < wordCount; i++) {
+			arr[i] = st.nextToken();
+		}
 		
+		int freq = 0;
+		String res = null;
+		String topWord[];
+		topWord = new String[4];
+		int topWordCount = 0;
+		
+		for (int i = 0; i < wordCount; i++) {
+	        int count = 0;
+	        for (int j = i + 1; j < wordCount; j++) {
+	            if (arr[j] == arr[i]) {
+	                count++;
+	            }
+	        }
+		
+	        if (count >= freq) {
+	            res = arr[i];
+	            freq = count;
+	            
+	            if(topWordCount < 4) {
+	            	topWord[topWordCount] = res;
+	            }
+	            else {
+	            	topWordCount = 0;
+	            	topWord[topWordCount] = res;
+	            }
+	        }
+	    }
+		
+		
+		logger.trace("The Most common words are: "+ topWord[0]+ topWord[1]+ topWord[2]+ topWord[3]+ topWord[4]);
 		logger.trace("buildTree completed");
-		return new NodeImpl();
+		return new NodeImpl('\0');
 	}
 
 	@Override
@@ -39,7 +80,7 @@ public class TrieImpl extends Trie {
 		logger.trace("deSerialize started");		
 		
 		logger.trace("deSerialize completed");	
-		return new NodeImpl();
+		return new NodeImpl('\0');
 	}
 
 	@Override
